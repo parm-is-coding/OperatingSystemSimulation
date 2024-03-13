e0660a4f3e9576fa07a48f3ed4824ee507baf31e
parm <parm@ubuntu20.myguest.virtualbox.org>
2024-03-13 11:15:50 -0700

    defined PCB and program termination

--- main.c
+++ main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include "helpers.h"
+bool initProcessIsAlive;
+
+
+typedef struct {
+    int PID;
+} ProcessControlBlock;
+
+int find(int){
+    return -1;
+}
 
 int main(){
     printf("Hello World\n");
+    ProcessControlBlock initProcess;
+
+    while(initProcessIsAlive){
+
+       
+
+        initProcessIsAlive = find(initProcess.PID) != -1;
+    }
+    printf("Simulation is Over");
     return 0;
 }
