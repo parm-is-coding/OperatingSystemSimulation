61409fecd6e8b0d2b4717030e6e9aed5955ff392
parm <parm@ubuntu20.myguest.virtualbox.org>
2024-03-20 10:28:56 -0700

    rm more uneeded files

--- scheduler.h
+++
@@ -1,8 +0,0 @@
-#ifndef scheduler.h
-#define scheduler.h
-#include "main.c"
-
-ProcessControlBlock* current;
-void runProcess();
-
-#endif
--- semaphore.h
+++
@@ -1,9 +0,0 @@
-#ifndef semaphore.h
-#define semaphore.h
-#include "main.c"
-
-void New_Sem();
-void Sem_P();
-void Sem_V();
-
-#endif
